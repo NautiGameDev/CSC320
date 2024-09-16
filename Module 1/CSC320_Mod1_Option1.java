bf17e96c881bd471e759bc896937ad81bc214b55
Nauti312 <chrisrussell167@gmail.com>
2024-09-15 20:50:01 -0600

    Source code.

---
+++ Module 1/CSC320_Mod1_Option1.java
@@ -0,0 +1,17 @@
+public class CSC320_Mod1_Option1 {
+    public static void main(String [] args) {
+
+        String firstName = "John";
+        String lastName = "Doe";
+        String address = "123 Somewhere Ln";
+        String city = "Nowhere, CO";
+        String zipCode = "12345";
+
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+        
+    }
+}
